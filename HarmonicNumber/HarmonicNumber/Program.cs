c3a5d850f070818ab4bc6ea6afdbcbd825a5bcf2
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-07-07 11:31:36 +0530

    [Rajesh] Added : Print the Nth Harmonic Number

---
+++ HarmonicNumber/HarmonicNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    class Harmonic
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Nth number to get Harmonic Number : ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            double harmonicNumber = 0;
+
+            if (Number > 0)
+            {
+                for (double i = 1; i <= Number; i++)
+                {
+                    harmonicNumber += 1 / i;
+                }
+                Console.WriteLine(Number + "th harmonic number is : " + harmonicNumber);
+                
+               
+            }
+            else
+            {
+                Console.WriteLine("Enter valid data : ");
+            }
+            
+
+
+        }
+    }
+}
